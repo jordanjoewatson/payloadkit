99eb9bd6ace36a880d1616b7f5a8ede6167e8f01
jord <jordwatson@live.com>
2022-07-17 19:21:55 +0100

    Update template.cpp

--- src/modules/cpp/xor/template.cpp
+++ src/modules/cpp/xor/template.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+
+// BYTE DATA
+
+unsigned char bytes[]=0x43,0x4d,0x30,0x30,0x0f,0x0D};
+
+// BYTE STRING
+
+unsigned char data[4]="\x41\x41\x41\x41";
+
+// BASE64 data
+
+char* base64_payload;
+
+int xor_base64()
+{
+  // example code placeholder
+}
+
+// BASE16 data
+
+char* base16_payload;
+
+int xor_base16()
+{
+  // example code placeholder
+}
+
+
